42385b1a7c8b957ffbddfb172c86d34c0fa05291
bouzuya <m@bouzuya.net>
2019-03-22 12:42:10 +0900

    fix ABC085.C

--- src/AtCoder/ABC085/C.purs
+++ src/AtCoder/ABC085/C.purs
@@ -5,6 +5,7 @@ module AtCoder.ABC085.C
 
 import Prelude
 
+import Control.Monad.Rec.Class as MonadRec
 import Control.MonadZero as MonadZero
 import Data.Array as Array
 import Data.Either (Either(..))
@@ -12,7 +13,6 @@ import Data.Either as Either
 import Data.Int as Int
 import Data.Maybe (Maybe(..))
 import Data.String as String
-import Data.Traversable as Traversable
 import Partial.Unsafe as Unsafe
 
 solve :: String -> String
@@ -27,17 +27,26 @@ solve' input = do
   n <- Either.note "n" (Int.fromString ns)
   y <- Either.note "y" (Int.fromString ys)
   pure
-    case Array.head (solve'' n y) of
+    case solve'' n y of
       Just { x, y: y', z } -> Array.intercalate " " (map show [x, y', z])
       Nothing -> "-1 -1 -1"
 
-solve'' :: Int -> Int -> Array { x :: Int, y :: Int, z :: Int }
-solve'' n y = do
-  x' <- Array.range n 0
-  MonadZero.guard (10000 * x' <= y)
-  y' <- Array.range (n - x') 0
-  MonadZero.guard (10000 * x' + 5000 * y' <= y)
-  let z' = n - x' - y'
-  MonadZero.guard (10000 * x' + 5000 * y' + 1000 * z' == y)
-  pure { x: x', y: y', z: z' }
+solve'' :: Int -> Int -> Maybe { x :: Int, y :: Int, z :: Int }
+solve'' n y = MonadRec.tailRec go n
+  where
+    go x'
+      | x' < 0 = MonadRec.Done Nothing
+      | otherwise =
+          case solve''' (n - x') (y - (10000 * x')) of
+            Just { y: y', z: z' } ->
+              MonadRec.Done (Just { x: x', y: y', z: z' })
+            Nothing -> MonadRec.Loop (x' - 1)
 
+solve''' :: Int -> Int -> Maybe { y :: Int, z :: Int }
+solve''' n y = MonadRec.tailRec go n
+  where
+    go y'
+      | y' < 0 = MonadRec.Done Nothing
+      | (5000 * y' + 1000 * (n - y')) == y =
+        MonadRec.Done (Just { y: y', z: n - y' })
+      | otherwise = MonadRec.Loop (y' - 1)
--- test/AtCoder/ABC085/C.purs
+++ test/AtCoder/ABC085/C.purs
@@ -5,7 +5,6 @@ module Test.AtCoder.ABC085.C
 import Prelude
 
 import AtCoder.ABC085.C as ABC085C
-import Data.Array as Array
 import Data.Maybe (Maybe(..))
 import Test.Unit (TestSuite)
 import Test.Unit as TestUnit
@@ -13,6 +12,7 @@ import Test.Unit.Assert as Assert
 
 tests :: TestSuite
 tests = TestUnit.suite "ABC085.C" do
+  let value { x, y, z } = 10000 * x + 5000 * y + 1000 * z
   TestUnit.test "example 1" do
     Assert.equal "4 0 5" (ABC085C.solve "9 45000\n")
 
@@ -22,10 +22,8 @@ tests = TestUnit.suite "ABC085.C" do
   TestUnit.test "example 3" do
     -- Assert.equal "14 27 959" (ABC085C.solve "1000 1234000\n")
     Assert.equal
-      (Just (10000 * 14 + 5000 * 27 + 1000 * 959))
-      (map
-        (\{ x, y, z } -> 10000 * x + 5000 * y + 1000 * z)
-        (Array.head (ABC085C.solve'' 1000 1234000)))
+      (Just (value { x: 14, y: 27, z: 959 }))
+      (map value (ABC085C.solve'' 1000 1234000))
 
   TestUnit.test "example 4" do
     Assert.equal "2000 0 0" (ABC085C.solve "2000 20000000\n")
