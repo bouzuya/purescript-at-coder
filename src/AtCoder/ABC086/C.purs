d2b2ec6f55ba7bb574932867c59973b752914520
bouzuya <m@bouzuya.net>
2019-03-23 17:12:11 +0900

    fix TLE

--- src/AtCoder/ABC086/C.purs
+++ src/AtCoder/ABC086/C.purs
@@ -20,19 +20,20 @@ solve input = Unsafe.unsafePartial (Either.fromRight (solve' input))
 solve' :: String -> Either String String
 solve' input = do
   lines <- pure (String.split (String.Pattern "\n") (String.trim input))
-  _ <- Either.note "N" ((Array.head lines) >>= Int.fromString)
+  n <- Either.note "N" ((Array.head lines) >>= Int.fromString)
   plans <-
-    Array.foldRecM
-      (\a txy ->
-        case String.split (String.Pattern " ") txy of
-          [ts, xs, ys] -> do
-            t <- Either.note "t" (Int.fromString ts)
-            x <- Either.note "x" (Int.fromString xs)
-            y <- Either.note "y" (Int.fromString ys)
-            pure (Array.snoc a { t, x, y })
-          _ -> Left ("invalid line: " <> txy))
-      []
-      (Array.drop 1 lines)
+    pure
+      (Array.mapMaybe
+        (\txy ->
+          case String.split (String.Pattern " ") txy of
+            [ts, xs, ys] -> do
+              t <- Int.fromString ts
+              x <- Int.fromString xs
+              y <- Int.fromString ys
+              pure { t, x, y }
+            _ -> Nothing)
+        (Array.drop 1 lines))
+  if (Array.length plans) /= n then Left "invalid lines" else pure unit
   pure (solve'' plans)
 
 solve'' :: Array { t :: Int, x :: Int, y :: Int } -> String
@@ -49,13 +50,11 @@ solve''' = (MonadRec.tailRec go) <<< ({ curr: { t: 0, x: 0, y: 0 }, plans: _ })
         Just { t: t', x: x', y: y' } ->
           let
             dt = t' - t
-            dx = x' - x
-            dy = y' - y
+            dx = Ord.abs (x' - x)
+            dy = Ord.abs (y' - y)
           in
-            if ((Ord.abs dx) + (Ord.abs dy) > dt) ||
-              (Int.odd (dt - ((Ord.abs dx) + (Ord.abs dy))))
-              then
-                MonadRec.Done false
+            if (dx + dy > dt) || (Int.odd (dt - (dx + dy)))
+              then MonadRec.Done false
               else
                 MonadRec.Loop
                   { curr: { t: t', x: x', y: y' }, plans: Array.drop 1 plans }
