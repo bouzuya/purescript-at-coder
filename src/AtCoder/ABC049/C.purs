fbb3044cd241a50f165cfe0287fe84b6967d22ea
bouzuya <m@bouzuya.net>
2019-03-23 16:23:30 +0900

    fix LTE

--- src/AtCoder/ABC049/C.purs
+++ src/AtCoder/ABC049/C.purs
@@ -4,10 +4,9 @@ module AtCoder.ABC049.C
 
 import Prelude
 
+import Control.Monad.Rec.Class as MonadRec
 import Data.Array as Array
 import Data.Maybe (Maybe(..))
-import Data.Set (Set)
-import Data.Set as Set
 import Data.String as String
 
 ts :: Array String
@@ -20,24 +19,22 @@ ts =
 
 solve :: String -> String
 solve input
-  | solve' (Set.fromFoldable [String.trim input]) = "YES"
+  | solve' [String.trim input] = "YES"
   | otherwise = "NO"
 
-solve' :: Set String -> Boolean
-solve' ss =
-  let
-    keys = Set.toUnfoldable ss
-  in
-    case Array.head keys of
-      Nothing -> false
-      Just s ->
-        let
-          nexts = solve'' s
-        in
-          case Array.find String.null nexts of
-            Just _ -> true
-            Nothing ->
-              solve' (Set.fromFoldable ((Array.drop 1 keys) <> nexts))
+solve' :: Array String -> Boolean
+solve' = MonadRec.tailRec go
+  where
+    go ss =
+      case Array.head ss of
+        Nothing -> MonadRec.Done false
+        Just s ->
+          let
+            nexts = solve'' s
+          in
+            case Array.find String.null nexts of
+              Just _ -> MonadRec.Done true
+              Nothing -> MonadRec.Loop (Array.nub ((Array.drop 1 ss) <> nexts))
 
 solve'' :: String -> Array String
 solve'' s =
