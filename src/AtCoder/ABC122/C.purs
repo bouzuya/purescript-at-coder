c020c1eff09a00dc37a317d102d826ba5ea18884
bouzuya <m@bouzuya.net>
2019-03-26 23:14:36 +0900

    fix TLE

--- src/AtCoder/ABC122/C.purs
+++ src/AtCoder/ABC122/C.purs
@@ -4,31 +4,46 @@ module AtCoder.ABC122.C
 
 import Prelude
 
+import Control.Monad.Rec.Class (Step)
 import Control.Monad.Rec.Class as MonadRec
+import Control.Monad.ST (ST)
 import Data.Array as Array
+import Data.Array.ST (STArray)
+import Data.Array.ST as STArray
 import Data.Either (Either(..))
 import Data.Either as Either
 import Data.Int as Int
 import Data.Maybe (Maybe(..))
+import Data.String (Pattern)
 import Data.String as String
 import Data.String.CodeUnits as CodeUnits
 import Data.Tuple (Tuple(..))
 import Partial.Unsafe as Unsafe
 
 counts :: String -> Array Int
-counts s = MonadRec.tailRec go { a: [], i: 0, n: 0 }
+counts s = STArray.run do
+  a <- STArray.unsafeThaw (Array.replicate (String.length s) 0)
+  MonadRec.tailRecM go { a, i: 0, n: 0 }
   where
+    ac :: Pattern
     ac = String.Pattern "AC"
-    go { i, a, n } =
-      case CodeUnits.indexOf' ac i s of
-        Nothing ->
-          MonadRec.Done (a <> (Array.replicate ((String.length s) - i) n))
-        Just i' ->
-          MonadRec.Loop
-            { i: i' + 1
-            , a: a <> (Array.replicate (i' - i + 1) n)
-            , n: n + 1
-            }
+
+    go :: forall h.
+      { a :: STArray h Int, i :: Int, n :: Int }
+      -> ST h (Step { a :: STArray h Int, i :: Int, n :: Int } (STArray h Int))
+    go { a, i, n } =
+      let
+        fill j j' = do
+          _ <- STArray.modify j (const n) a
+          if j == j' then pure a else fill (j + 1) j'
+      in
+        case CodeUnits.indexOf' ac i s of
+          Nothing -> do
+            _ <- fill i ((String.length s) - 1)
+            pure (MonadRec.Done a)
+          Just i' -> do
+            _ <- fill i i'
+            pure (MonadRec.Loop { a, i: i' + 1, n: n + 1 })
 
 countRange :: Array Int -> Int -> Int -> Int
 countRange a l r =
