69a98d9c3191db5843772d6575471a8970b36edf
bouzuya <m@bouzuya.net>
2019-03-26 17:59:53 +0900

    fix TLE (fail)

--- src/AtCoder/ABC122/C.purs
+++ src/AtCoder/ABC122/C.purs
@@ -10,26 +10,33 @@ import Data.Either (Either(..))
 import Data.Either as Either
 import Data.Int as Int
 import Data.Maybe (Maybe(..))
-import Data.Maybe as Maybe
 import Data.String as String
 import Data.String.CodeUnits as CodeUnits
 import Data.Tuple (Tuple(..))
 import Partial.Unsafe as Unsafe
 
-indices :: String -> Array Int
-indices s = MonadRec.tailRec go { i: 0, a: [] }
+counts :: String -> Array Int
+counts s = MonadRec.tailRec go { a: [], i: 0, n: 0 }
   where
     ac = String.Pattern "AC"
-    go { i, a } =
+    go { i, a, n } =
       case CodeUnits.indexOf' ac i s of
-        Nothing -> MonadRec.Done a
-        Just i' -> MonadRec.Loop { i: i' + 1, a: Array.snoc a i' }
+        Nothing ->
+          MonadRec.Done (a <> (Array.replicate ((String.length s) - i) n))
+        Just i' ->
+          MonadRec.Loop
+            { i: i' + 1
+            , a: a <> (Array.replicate (i' - i + 1) n)
+            , n: n + 1
+            }
 
-countIndices :: Array Int -> Int -> Int -> Int
-countIndices a l r = Maybe.fromMaybe 0 do
-  i1 <- Array.findIndex (l <= _) a
-  i2 <- Array.findLastIndex (_ <= r) a
-  pure (i2 - i1 + 1)
+countRange :: Array Int -> Int -> Int -> Int
+countRange a l r =
+  let
+    i1 = Unsafe.unsafePartial (Array.unsafeIndex a l)
+    i2 = Unsafe.unsafePartial (Array.unsafeIndex a r)
+  in
+    i2 - i1
 
 parseLine :: String -> Maybe (Tuple Int Int)
 parseLine line =
@@ -50,16 +57,17 @@ solve'' :: String -> Either String String
 solve'' input = do
   let lines = String.split (String.Pattern "\n") (String.trim input)
   (Tuple n q) <- Either.note "N Q" (parseLine =<< (Array.head lines))
-  s <- Either.note "S" (Array.head (Array.drop 1 lines))
+  s <- Either.note "S" (Array.head =<< (Array.tail (Array.take 2 lines)))
   let
-    count = countIndices (indices s)
+    c = counts s
+    count = countRange c
     as =
       Array.mapMaybe
         (\qline -> do
           (Tuple l r) <- parseLine qline
           let
             il = l - 1
-            ir = r - 2
+            ir = r - 1
           pure (show (count il ir)))
         (Array.drop 2 lines)
   if Array.length as /= q then Left "as length" else pure unit
