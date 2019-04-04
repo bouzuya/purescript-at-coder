3f4f9f4196abef27bc4e4fb35b61ecc8e554a5cb
bouzuya <m@bouzuya.net>
2019-04-04 10:30:31 +0900

    fix TLE (fail)

--- src/AtCoder/ABC118/C.purs
+++ src/AtCoder/ABC118/C.purs
@@ -4,13 +4,19 @@ module AtCoder.ABC118.C
 
 import Prelude
 
+import Control.Monad.Rec.Class (Step)
+import Control.Monad.Rec.Class as MonadRec
+import Control.Monad.ST (ST)
+import Control.Monad.ST as ST
 import Data.Array as Array
+import Data.Array.ST (STArray)
+import Data.Array.ST as STArray
 import Data.Either (Either(..))
 import Data.Either as Either
 import Data.Int as Int
 import Data.Maybe (Maybe(..))
-import Data.Maybe as Maybe
 import Data.String as String
+import Data.Tuple (Tuple(..))
 import Partial.Unsafe as Unsafe
 
 solve :: String -> String
@@ -29,28 +35,36 @@ solve' input = do
   pure (show (g as))
 
 g :: Array Int -> Int
-g xs
-  | Array.length xs == 1 =
-    Unsafe.unsafePartial (Maybe.fromJust (Array.head xs))
-  | otherwise = g (f xs)
-
-f :: Array Int -> Array Int
-f xs =
-  let
-    xs' = Array.reverse (Array.sort xs)
-    x = Unsafe.unsafePartial (Array.unsafeIndex xs' 0)
-    os = Array.drop 1 xs'
-    m =
-      Array.foldl
-        (\acc o ->
-          case acc, (x `mod` o) of
-            Nothing, 0 -> Nothing
-            Nothing, m' -> Just m'
-            Just acc', 0 -> Just acc'
-            Just acc', m' -> Just (min acc' m'))
-        Nothing
-        os
-  in
-    case m of
-      Nothing -> os
-      Just m' -> Array.snoc os m'
+g xs = ST.run do
+    sta <- STArray.unsafeThaw xs
+    MonadRec.tailRecM go sta
+  where
+    go :: forall h. STArray h Int -> ST h (Step (STArray h Int) Int)
+    go sta = do
+      a <- STArray.unsafeFreeze sta
+      case a of
+        [t] -> pure (MonadRec.Done t)
+        _ -> do
+          (Tuple index v) <-
+            pure
+              (Array.foldl
+                (\ta@(Tuple _ ia) tb@(Tuple _ ib) -> if ia < ib then tb else ta)
+                bottom
+                (Array.mapWithIndex Tuple a))
+          _ <- STArray.splice index 1 [] sta
+          os <- STArray.unsafeFreeze sta
+          let
+            m =
+              Array.foldl
+                (\acc o ->
+                  case acc, (v `mod` o) of
+                    Nothing, 0 -> Nothing
+                    Nothing, m' -> Just m'
+                    Just acc', 0 -> Just acc'
+                    Just acc', m' -> Just (min acc' m'))
+                Nothing
+                os
+          case m of
+            Nothing -> pure unit
+            Just m' -> void (STArray.push m' sta)
+          pure (MonadRec.Loop sta)
