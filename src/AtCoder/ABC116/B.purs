e97cb03ccdbde308f6812c773292e6c90d9f0a1f
bouzuya <m@bouzuya.net>
2019-04-05 14:10:23 +0900

    fix ABC116.B

--- src/AtCoder/ABC116/B.purs
+++ src/AtCoder/ABC116/B.purs
@@ -4,9 +4,12 @@ module AtCoder.ABC116.B
 
 import Prelude
 
+import Control.Monad.Rec.Class (Step)
+import Control.Monad.Rec.Class as MonadRec
+import Control.Monad.ST (ST)
 import Control.Monad.ST as ST
-import Control.Monad.ST.Internal as STRef
 import Data.Array as Array
+import Data.Array.ST (STArray)
 import Data.Array.ST as STArray
 import Data.Either (Either(..))
 import Data.Either as Either
@@ -28,30 +31,21 @@ solve' input = do
 
 solve'' :: Int -> Int
 solve'' s = ST.run do
-  let length = 1000000
-  sta <- STArray.unsafeThaw (Array.replicate length s)
-  mRef <- STRef.new 2
-  ST.while
-    (do
-      m <- STRef.read mRef
-      if m == length
-        then pure false
+  sta <- STArray.empty
+  _ <- STArray.push s sta
+  MonadRec.tailRecM go sta
+  where
+    go :: forall h. STArray h Int -> ST h (Step (STArray h Int) Int)
+    go sta = do
+      a <- STArray.unsafeFreeze sta
+      prevMaybe <- STArray.peek ((Array.length a) - 1) sta
+      prev <- Maybe.maybe' (\_ -> Unsafe.unsafeCrashWith "prev") pure prevMaybe
+      let am = f prev
+      if Maybe.isJust (Array.findIndex (eq am) a)
+        then pure (MonadRec.Done ((Array.length a) + 1))
         else do
-          prevMaybe <- STArray.peek (m - 1 - 1) sta
-          prev <-
-            Maybe.maybe' (\_ -> Unsafe.unsafeCrashWith "prev") pure prevMaybe
-          let am = f prev
-          _ <- STArray.poke (m - 1) am sta
-          a <- STArray.unsafeFreeze sta
-          found <-
-            pure (Maybe.isJust (Array.find (eq am) (Array.take (m - 1) a)))
-          if found
-            then pure false
-            else do
-              _ <- STRef.write (m + 1) mRef
-              pure true)
-    (pure unit)
-  STRef.read mRef
+          _ <- STArray.push am sta
+          pure (MonadRec.Loop sta)
 
 f :: Int -> Int
 f n
