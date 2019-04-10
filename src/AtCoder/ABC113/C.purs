cba69b629dfde905d16d6f4ae239359b345db18d
bouzuya <m@bouzuya.net>
2019-04-10 14:54:56 +0900

    fix TLE

--- src/AtCoder/ABC113/C.purs
+++ src/AtCoder/ABC113/C.purs
@@ -5,6 +5,7 @@ module AtCoder.ABC113.C
 import Prelude
 
 import Control.Monad.ST as ST
+import Control.Monad.ST.Ref as STRef
 import Data.Array as Array
 import Data.Array.ST as STArray
 import Data.Either (Either)
@@ -47,43 +48,40 @@ solve' input = do
               (Unsafe.unsafePartial (Array.unsafeIndex lines (i + 1)))) of
           [p, y] -> void (STArray.push { p, y, i } sta)
           _ -> Unsafe.unsafeCrashWith "p y line"
+      _ <-
+        STArray.sortBy
+          (\{ y: y1, p: p1 } { y: y2, p: p2 } ->
+            case compare p1 p2 of
+              LT -> LT
+              GT -> GT
+              EQ -> compare y1 y2)
+          sta
       STArray.unsafeFreeze sta
-  pure (String.joinWith "\n" (solve'' n m pys))
-
-solve'' :: Int -> Int -> Array { p :: Int, y :: Int, i :: Int } -> Array String
-solve'' n m pys = ST.run do
-  stpys <- STArray.unsafeThaw pys
-  _ <-
-    STArray.sortBy
-      (\{ y: y1, p: p1 } { y: y2, p: p2 } ->
-        case compare p1 p2 of
-          LT -> LT
-          GT -> GT
-          EQ -> compare y1 y2)
-      stpys
-  pys' <- STArray.unsafeFreeze stpys
-  sta <- STArray.unsafeThaw (Array.replicate m "")
-  _ <-
-    Array.foldRecM
-      (\{ p', x } { p, i } -> do
+    cs = ST.run do
+      sta <- STArray.unsafeThaw (Array.replicate m "")
+      pRef <- STRef.new 0
+      xRef <- STRef.new 0
+      ST.foreach pys \{ p, i } -> do
+        p' <- STRef.read pRef
+        x <- STRef.read xRef
         let x' = (if p' /= p then 0 else x) + 1
         _ <- STArray.poke i (pad p <> pad x') sta
-        pure { p': p, x: x' })
-      { p': -1, x: 0 }
-      pys'
-  STArray.unsafeFreeze sta
+        _ <- STRef.write p pRef
+        _ <- STRef.write x' xRef
+        pure unit
+      STArray.unsafeFreeze sta
+  pure (String.joinWith "\n" cs)
 
-  where
-    pad :: Int -> String
-    pad x =
-      let
-        s = show x
-      in
-        case CodeUnits.length s of
-          0 -> "000000"
-          1 -> "00000" <> s
-          2 -> "0000" <> s
-          3 -> "000" <> s
-          4 -> "00" <> s
-          5 -> "0" <> s
-          _ -> s
+pad :: Int -> String
+pad x =
+  let
+    s = show x
+  in
+    case CodeUnits.length s of
+      0 -> "000000"
+      1 -> "00000" <> s
+      2 -> "0000" <> s
+      3 -> "000" <> s
+      4 -> "00" <> s
+      5 -> "0" <> s
+      _ -> s
