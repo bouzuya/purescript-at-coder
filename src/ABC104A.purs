448f99182af294bb22ba394a3748255cc402ea80
bouzuya <m@bouzuya.net>
2019-05-02 20:15:20 +0900

    ABC104A

---
+++ src/ABC104A.purs
@@ -0,0 +1,58 @@
+-- abc104_a
+module ABC104A
+  ( main
+  , solve
+  ) where
+
+import Prelude
+
+import Data.Either as Either
+import Data.Int as Int
+import Data.String as String
+import Effect (Effect)
+import Effect.Aff as Aff
+import Effect.Class as Class
+import Effect.Ref as Ref
+import Node.Encoding as Encoding
+import Node.Process as Process
+import Node.Stream as Stream
+import Partial.Unsafe as Unsafe
+
+main :: Effect Unit
+main = Aff.launchAff_ do
+  input <- readStdin
+  output <- pure (solve input)
+  Class.liftEffect (writeStdout output)
+
+readStdin :: Aff.Aff String
+readStdin =
+  let r = Process.stdin
+  in
+    Aff.makeAff
+      (\callback -> do
+        ref <- Ref.new ""
+        Stream.onDataString r Encoding.UTF8 \s -> do
+          buffer <- Ref.read ref
+          Ref.write (buffer <> s) ref
+        Stream.onEnd r do
+          buffer <- Ref.read ref
+          callback (pure buffer)
+        pure mempty)
+
+writeStdout :: String -> Effect Unit
+writeStdout s =
+  void (Stream.writeString Process.stdout Encoding.UTF8 s (pure unit))
+
+splitBySP :: String -> Array String
+splitBySP s = String.split (String.Pattern " ") s
+
+solve :: String -> String
+solve input = Either.either (\s -> Unsafe.unsafeCrashWith s) identity do
+  r <- Either.note "R" (Int.fromString (String.trim input))
+  pure ((solve' r) <> "\n")
+
+solve' :: Int -> String
+solve' r
+  | r < 1200 = "ABC"
+  | r < 2800 = "ARC"
+  | otherwise = "AGC"
