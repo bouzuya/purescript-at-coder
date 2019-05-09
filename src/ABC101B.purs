ac1ea76d574041abed92ec9ff16e4b79d603fc8f
bouzuya <m@bouzuya.net>
2019-05-09 23:06:30 +0900

    ABC101B

---
+++ src/ABC101B.purs
@@ -0,0 +1,67 @@
+-- abc101_b
+module ABC101B
+  ( main
+  , solve
+  ) where
+
+import Prelude
+
+import Data.Array as Array
+import Data.Either as Either
+import Data.Foldable as Foldable
+import Data.Int as Int
+import Data.String as String
+import Data.String.CodeUnits as CodeUnits
+import Effect (Effect)
+import Effect.Aff as Aff
+import Effect.Class as Class
+import Effect.Ref as Ref
+import Node.Encoding as Encoding
+import Node.Process as Process
+import Node.Stream as Stream
+import Partial.Unsafe as Unsafe
+
+main :: Effect Unit
+main = Aff.launchAff_ do
+  input <- readStdin
+  output <- pure (solve input)
+  Class.liftEffect (writeStdout output)
+
+readStdin :: Aff.Aff String
+readStdin =
+  let r = Process.stdin
+  in
+    Aff.makeAff
+      (\callback -> do
+        ref <- Ref.new ""
+        Stream.onDataString r Encoding.UTF8 \s -> do
+          buffer <- Ref.read ref
+          Ref.write (buffer <> s) ref
+        Stream.onEnd r do
+          buffer <- Ref.read ref
+          callback (pure buffer)
+        pure mempty)
+
+writeStdout :: String -> Effect Unit
+writeStdout s =
+  void (Stream.writeString Process.stdout Encoding.UTF8 s (pure unit))
+
+toYesNo :: Boolean -> String
+toYesNo yes
+  | yes = "Yes"
+  | otherwise = "No"
+
+solve :: String -> String
+solve input = Either.either (\s -> Unsafe.unsafeCrashWith s) identity do
+  n <- Either.note "n" (Int.fromString (String.trim input))
+  pure ((toYesNo (solve' n)) <> "\n")
+
+solve' :: Int -> Boolean
+solve' n =
+  let
+    n' =
+      Foldable.sum
+        (Array.mapMaybe
+          (Int.fromString <<< CodeUnits.singleton)
+          (CodeUnits.toCharArray (show n)))
+  in (n `mod` n') == 0
