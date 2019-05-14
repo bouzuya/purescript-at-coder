509bbfb004e2ec2ac0d42cd8e1637e548e852384
bouzuya <m@bouzuya.net>
2019-05-14 23:03:20 +0900

    ABC098A

---
+++ src/ABC098A.purs
@@ -0,0 +1,66 @@
+-- abc098_a
+module ABC098A
+  ( main
+  , solve
+  ) where
+
+import Prelude
+
+import Data.Array as Array
+import Data.Either as Either
+import Data.Int as Int
+import Data.Maybe (Maybe)
+import Data.Maybe as Maybe
+import Data.String as String
+import Data.Tuple (Tuple(..))
+import Data.Tuple as Tuple
+import Effect (Effect)
+import Effect.Aff as Aff
+import Effect.Class as Class
+import Effect.Ref as Ref
+import Node.Encoding as Encoding
+import Node.Process as Process
+import Node.Stream as Stream
+import Partial.Unsafe as Unsafe
+
+main :: Effect Unit
+main = Aff.launchAff_ do
+  input <- readStdin
+  output <- pure (solve input)
+  Class.liftEffect (writeStdout output)
+
+readStdin :: Aff.Aff String
+readStdin =
+  let r = Process.stdin
+  in
+    Aff.makeAff
+      (\callback -> do
+        ref <- Ref.new ""
+        Stream.onDataString r Encoding.UTF8 \s -> do
+          buffer <- Ref.read ref
+          Ref.write (buffer <> s) ref
+        Stream.onEnd r do
+          buffer <- Ref.read ref
+          callback (pure buffer)
+        pure mempty)
+
+writeStdout :: String -> Effect Unit
+writeStdout s =
+  void (Stream.writeString Process.stdout Encoding.UTF8 s (pure unit))
+
+splitBySP :: String -> Array String
+splitBySP s = String.split (String.Pattern " ") s
+
+int2 :: String -> Maybe (Tuple Int Int)
+int2 s =
+  case Array.mapMaybe Int.fromString (splitBySP s) of
+    [a, b] -> Maybe.Just (Tuple.Tuple a b)
+    _ -> Maybe.Nothing
+
+solve :: String -> String
+solve input = Either.either (\s -> Unsafe.unsafeCrashWith s) identity do
+  Tuple.Tuple a b <- Either.note "a b" (int2 (String.trim input))
+  pure ((show (solve' a b)) <> "\n")
+
+solve' :: Int -> Int -> Int
+solve' a b = max (a + b) (max (a - b) (a * b))
