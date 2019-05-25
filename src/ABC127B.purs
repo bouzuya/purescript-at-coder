96f77c2b6f536314622fe80d795a123c8b226329
bouzuya <m@bouzuya.net>
2019-05-25 21:15:07 +0900

    ABC127B

---
+++ src/ABC127B.purs
@@ -0,0 +1,70 @@
+-- abc127_b
+module ABC127B
+  ( main
+  , solve
+  ) where
+
+import Prelude
+
+import Data.Array as Array
+import Data.Either as Either
+import Data.Int as Int
+import Data.Maybe (Maybe)
+import Data.Maybe as Maybe
+import Data.String as String
+import Data.Tuple (Tuple)
+import Data.Tuple as Tuple
+import Effect (Effect)
+import Effect.Aff as Aff
+import Effect.Class as Class
+import Effect.Ref as Ref
+import Node.Encoding as Encoding
+import Node.Process as Process
+import Node.Stream as Stream
+import Partial.Unsafe as Unsafe
+
+main :: Effect Unit
+main = Aff.launchAff_ do
+  input <- readStdin
+  output <- pure (solve input)
+  Class.liftEffect (writeStdout output)
+
+readStdin :: Aff.Aff String
+readStdin =
+  let r = Process.stdin
+  in
+    Aff.makeAff
+      (\callback -> do
+        ref <- Ref.new ""
+        Stream.onDataString r Encoding.UTF8 \s -> do
+          buffer <- Ref.read ref
+          Ref.write (buffer <> s) ref
+        Stream.onEnd r do
+          buffer <- Ref.read ref
+          callback (pure buffer)
+        pure mempty)
+
+writeStdout :: String -> Effect Unit
+writeStdout s =
+  void (Stream.writeString Process.stdout Encoding.UTF8 s (pure unit))
+
+splitBySP :: String -> Array String
+splitBySP s = String.split (String.Pattern " ") s
+
+int2 :: String -> Maybe (Tuple Int Int)
+int2 s =
+  case Array.mapMaybe Int.fromString (splitBySP s) of
+    [a, b] -> Maybe.Just (Tuple.Tuple a b)
+    _ -> Maybe.Nothing
+
+solve :: String -> String
+solve input = Either.either (\s -> Unsafe.unsafeCrashWith s) identity do
+  { r, d, x } <-
+    Either.note "r d x"
+      case Array.mapMaybe Int.fromString (splitBySP (String.trim input)) of
+        [r, d, x] -> Maybe.Just { r, d, x }
+        _ -> Maybe.Nothing
+  pure ((String.joinWith "\n" (map show (solve' r d x))) <> "\n")
+
+solve' :: Int -> Int -> Int -> Array Int
+solve' r d x = Array.scanl (\p _ -> r * p - d) x (Array.range 1 10)
