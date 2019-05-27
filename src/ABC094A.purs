428de0af5b8f2ba7b4632c728bc72d8762194cca
bouzuya <m@bouzuya.net>
2019-05-27 23:13:08 +0900

    ABC094A

---
+++ src/ABC094A.purs
@@ -0,0 +1,64 @@
+-- abc094_a
+module ABC094A
+  ( main
+  , solve
+  ) where
+
+import Prelude
+
+import Data.Array as Array
+import Data.Either as Either
+import Data.Int as Int
+import Data.String as String
+import Effect (Effect)
+import Effect.Aff as Aff
+import Effect.Class as Class
+import Effect.Ref as Ref
+import Node.Encoding as Encoding
+import Node.Process as Process
+import Node.Stream as Stream
+import Partial.Unsafe as Unsafe
+
+main :: Effect Unit
+main = Aff.launchAff_ do
+  input <- readStdin
+  output <- pure (solve input)
+  Class.liftEffect (writeStdout output)
+
+readStdin :: Aff.Aff String
+readStdin =
+  let r = Process.stdin
+  in
+    Aff.makeAff
+      (\callback -> do
+        ref <- Ref.new ""
+        Stream.onDataString r Encoding.UTF8 \s -> do
+          buffer <- Ref.read ref
+          Ref.write (buffer <> s) ref
+        Stream.onEnd r do
+          buffer <- Ref.read ref
+          callback (pure buffer)
+        pure mempty)
+
+writeStdout :: String -> Effect Unit
+writeStdout s =
+  void (Stream.writeString Process.stdout Encoding.UTF8 s (pure unit))
+
+splitBySP :: String -> Array String
+splitBySP s = String.split (String.Pattern " ") s
+
+toYesNo :: Boolean -> String
+toYesNo yes
+  | yes = "YES"
+  | otherwise = "NO"
+
+solve :: String -> String
+solve input = Either.either (\s -> Unsafe.unsafeCrashWith s) identity do
+  { a, b, x } <-
+    case Array.mapMaybe Int.fromString (splitBySP (String.trim input)) of
+      [a, b, x] -> Either.Right { a, b, x }
+      _ -> Either.Left "a b x"
+  pure ((toYesNo (solve' a b x)) <> "\n")
+
+solve' :: Int -> Int -> Int -> Boolean
+solve' a b x = between a (a + b) x
