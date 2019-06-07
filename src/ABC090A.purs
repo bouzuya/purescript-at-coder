a762d867978b300a32694fd916a0b215a5af9462
bouzuya <m@bouzuya.net>
2019-06-07 22:18:24 +0900

    ABC090A

---
+++ src/ABC090A.purs
@@ -0,0 +1,55 @@
+-- abc090_a
+module ABC090A
+  ( main
+  , solve
+  ) where
+
+import Prelude
+
+import Data.Array as Array
+import Data.Either as Either
+import Data.String as String
+import Data.String.CodeUnits as CodeUnits
+import Effect (Effect)
+import Effect.Aff as Aff
+import Effect.Class as Class
+import Effect.Ref as Ref
+import Node.Encoding as Encoding
+import Node.Process as Process
+import Node.Stream as Stream
+import Partial.Unsafe as Unsafe
+
+main :: Effect Unit
+main = Aff.launchAff_ do
+  input <- readStdin
+  output <- pure (solve input)
+  Class.liftEffect (writeStdout output)
+
+readStdin :: Aff.Aff String
+readStdin =
+  let r = Process.stdin
+  in
+    Aff.makeAff
+      (\callback -> do
+        ref <- Ref.new ""
+        Stream.onDataString r Encoding.UTF8 \s -> do
+          buffer <- Ref.read ref
+          Ref.write (buffer <> s) ref
+        Stream.onEnd r do
+          buffer <- Ref.read ref
+          callback (pure buffer)
+        pure mempty)
+
+writeStdout :: String -> Effect Unit
+writeStdout s =
+  void (Stream.writeString Process.stdout Encoding.UTF8 s (pure unit))
+
+splitByNL :: String -> Array String
+splitByNL s = String.split (String.Pattern "\n") (String.trim s)
+
+solve :: String -> String
+solve input = Either.either (\s -> Unsafe.unsafeCrashWith s) identity do
+  let
+    cs = Array.filter (notEq '\n') (CodeUnits.toCharArray input)
+    cs' = CodeUnits.fromCharArray (Array.mapMaybe (Array.index cs) [0, 4, 8])
+  pure (cs' <> "\n")
