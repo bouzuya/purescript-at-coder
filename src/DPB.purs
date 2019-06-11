7461d4c0101cc5b624a7d7e6d6bbe894850ee530
bouzuya <m@bouzuya.net>
2019-06-11 23:00:11 +0900

    DPB (TLE)

--- src/DPB.purs
+++ src/DPB.purs
@@ -8,7 +8,6 @@ import Prelude
 
 import Control.Monad.ST (ST)
 import Control.Monad.ST as ST
-import Control.Monad.ST.Internal as STRef
 import Data.Array as Array
 import Data.Array.Partial as ArrayPartial
 import Data.Array.ST (STArray)
@@ -22,34 +21,18 @@ import Data.String as String
 import Data.Tuple (Tuple)
 import Data.Tuple as Tuple
 import Effect (Effect)
-import Effect.Aff as Aff
 import Effect.Class as Class
-import Effect.Ref as Ref
 import Node.Encoding as Encoding
+import Node.FS.Sync as FS
 import Node.Process as Process
 import Node.Stream as Stream
 import Partial.Unsafe as Unsafe
 
 main :: Effect Unit
-main = Aff.launchAff_ do
-  input <- readStdin
+main = do
+  input <- FS.readTextFile Encoding.UTF8 "/dev/stdin"
   output <- pure (solve input)
   Class.liftEffect (writeStdout output)
-
-readStdin :: Aff.Aff String
-readStdin =
-  let r = Process.stdin
-  in
-    Aff.makeAff
-      (\callback -> do
-        ref <- Ref.new ""
-        Stream.onDataString r Encoding.UTF8 \s -> do
-          buffer <- Ref.read ref
-          Ref.write (buffer <> s) ref
-        Stream.onEnd r do
-          buffer <- Ref.read ref
-          callback (pure buffer)
-        pure mempty)
 
 writeStdout :: String -> Effect Unit
 writeStdout s =
